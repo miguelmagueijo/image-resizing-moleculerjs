cbfe39feff9f0db4ef0ed7bb7557672ddeac57b2
Miguel Magueijo <miguelmagueijo@hotmail.com>
2023-02-04 19:32:12 +0000

    Create HTTP server to UI, use env variables

--- API/services/api.service.js
+++ API/services/api.service.js
@@ -6,10 +6,13 @@ module.exports = {
 	name: "api",
     mixins: [ApiService],
     settings: {
+        ip: process.env.API_HOST || "0.0.0.0",
+        port: process.env.API_PORT || 3000,
         routes: [{
             whitelist: [
                 "image.resize"
             ],
+            cors: { origin: `http://${process.env.UI_HOST}:${process.env.UI_PORT}`, methods: ["GET"],  allowedHeaders: [], exposedHeaders: [], credentials: false, maxAge: 3600 },
             aliases: {
                 "GET api/image": "image.resize"
             },
---
+++ UI/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
+const host = process.env.UI_HOST || "localhost";
+const port = process.env.UI_PORT || 8000;
+
+async function sendFile(res, filename, type) {
+    try {
+        const data = await fs.readFile(filename);
+        res.writeHead(200, { "Content-Type": type });
+        res.end(data);
+    } catch (e) {
+        res.statusCode = 404;
+        res.end();
+    }
+}
+
+function main() {
+
+    const requestListener = async (req, res) => {
+        switch(req.url) {
+            case "/":
+                await sendFile(res, path.join(__dirname, "index.html"), "text/html");
+                break;
+            case "/public/css/bulma.min.css":
+                await sendFile(res, path.join(__dirname, "public", "css", "bulma.min.css"), "text/css");
+                break;
+            case "/public/js/request.js":
+                await sendFile(res, path.join(__dirname, "public", "js", "request.js"), "text/js");
+                break;
+            default:
+                res.statusCode = 404;
+                res.end();
+        }
+
+    };
+
+    const server = http.createServer(requestListener);
+
+    server.listen(port, host, () => {
+        console.log(`Server on! http://${host}:${port}`);
+    });
+}
+
+main()
