ee2e74c5a8800a31867d2a4e36a4ae03f77dd4e2
Miguel Magueijo <miguelmagueijo@hotmail.com>
2023-02-05 02:49:08 +0000

    fix fs usage (use await instead of functs as args)

--- UI/index.js
+++ UI/index.js
@@ -17,29 +17,24 @@ async function sendFile(res, filename, type) {
     }
 }
 
-function main() {
+async function main() {
     const API_PORT = process.env.API_PORT;
     const htmlPath = path.join(__dirname, "index.html")
     const jsPath = path.join(__dirname, "public", "js", "request.js");
     const cssPath = path.join(__dirname, "public", "css", "bulma.min.css");
 
-    if (!isNaN(Number(API_PORT))) {~
-        console.log(`API port changed! New port: ${API_PORT}`);
-        fs.readFile(jsPath, (err, data) => {
-            if (err) {
-                console.error(err);
-                throw new Error("Something went wrong while opening request.js.");
-            }
+    if (!isNaN(Number(API_PORT))) {
+        console.log(`API port changed! New port: ${API_PORT}. Changing request.js...`);
 
-            const finalFile = data.replace(/http:\/\/127.0.0.1:3000\/api\/image/g, `http://127.0.0.1:${API_PORT}/api/image`);
-
-            fs.writeFile(jsPath, finalFile, "utf8", (err) => {
-                if (err) {
-                    console.error(err);
-                    throw new Error("Something went wrong while saving request.js");
-                }
-            })
-        });
+        try {
+            const oldFile = await fs.readFile(jsPath);
+            const finalBuffer = Buffer.from(oldFile.toString().replace(/http:\/\/127.0.0.1:3000\/api\/image/g, `http://127.0.0.1:${API_PORT}/api/image`), "utf-8");
+            await fs.writeFile(jsPath, finalBuffer);
+            console.log("Changed request.js with success!");
+        } catch (e) {
+            console.error("Something went wrong changing request.js");
+            throw e;
+        }
     }
 
     const requestListener = async (req, res) => {
@@ -67,4 +62,4 @@ function main() {
     });
 }
 
-main()
+main().then((e) => console.log("Ready for action!"))
